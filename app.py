f8ae8bca297eedccf8b50f83b7a30ec68b9a8cc9
vishnu venu <vishnuvenuvenk@gmail.com>
2019-09-30 15:46:04 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,56 @@
+from flask import Flask, request, jsonify
+import json
+from waitress import serve
+from datetime import timedelta
+import os
+import smtplib
+import ssl
+from email.message import EmailMessage
+
+
+from flask_cors import CORS
+
+
+app = Flask(__name__)
+CORS(app)
+
+
+PORT = 8080
+
+
+@app.route("/invite_user", methods=['POST'])
+def user_invite(receiver_email, subject, message, success=True):
+    req = request.json
+
+    receiver_email = req["receiver_email"]
+    subject = req["subject"]
+    message = req["message"]
+
+    port = 465  # For SSL
+    sender_email = "mail@jaaga.in"
+    password = "mail@jaaga"
+    dev_email = "labs@jaaga.in"
+    msg = EmailMessage()
+    msg['Subject'] = subject
+    msg['From'] = sender_email
+    msg.set_content(message)
+    # Create a secure SSL context
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+        server.login(sender_email, password)
+        if success:
+            msg['To'] = receiver_email
+            server.send_message(msg)
+        else:
+            msg['To'] = receiver_email + ',' + dev_email
+            server.send_message(msg)
+
+    # return jsonify(result)
+
+    return "work"
+
+
+if __name__ == "__main__":
+    # app.run(debug=True)
+    serve(app, listen='*:{}'.format(str(PORT)))
