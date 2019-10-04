4e5e13a7d41197c9a66895a470f0da831c62214a
vishnu venu <vishnuvenuvenk@gmail.com>
2019-10-04 19:34:35 +0530

    added graphql client 7

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ def user_invite():
     try:
         graphqlClient.execute(user_insert_mutation, {
             'objects': list(users)})
-        return "not ok"
+        # return "not ok"
 
     except:
         return "not work"
