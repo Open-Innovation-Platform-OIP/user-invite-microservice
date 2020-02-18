878af56d399f4cf49a2988448832e273999852ee
Tej Pochiraju <tej@jaaga.in>
2020-02-18 14:35:05 +0530

    added build script

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# access_token = 5b5efa54-6113-48e2-9053-79ce9b3b04f4
+NAME_SPACE="socialalphaoip"
+IMAGE_NAME="user-invite-microservice"
+TAG="dev"
+docker login --username tejpochiraju --password 5b5efa54-6113-48e2-9053-79ce9b3b04f4
+docker build -t $NAME_SPACE/$IMAGE_NAME:$TAG .
+docker push $NAME_SPACE/$IMAGE_NAME:$TAG
